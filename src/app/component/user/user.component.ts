49a9ecbd36686cafe148fa785667fa00fdaa13a5
mustafamemon <mustafmemon360@gmail.com>
2025-01-15 17:55:03 +0500

    user

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-user';
 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+selectedUser = DUMMY_USERS[randomIndex]
 }
