47f4f8e8ef8f67a98c9d74382aadc2fe289228be
mustafamemon <mustafmemon360@gmail.com>
2025-01-17 18:03:04 +0500

    Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./component/header/header.component";
 import { UserComponent } from "./component/user/user.component";
+import { DUMMY_USERS } from './dummy-user';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [ HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'practice';
+
+  users = DUMMY_USERS;
+
+  onSelectedUser(id: any){
+console.warn("selected user", id)
+  }
 }
--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  Output,
+  signal,
+} from '@angular/core';
 import { DUMMY_USERS } from '../../dummy-user';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-selectedUser = DUMMY_USERS[randomIndex]
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectedUser() {
+    this.select.emit(this.id)
+  }
 }
