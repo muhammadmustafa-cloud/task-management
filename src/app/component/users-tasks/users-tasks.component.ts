0a35089f721fb7838a740e45b7df1784dca960d8
mustafamemon <mustafmemon360@gmail.com>
2025-01-25 15:01:00 +0500

    58

--- src/app/component/users-tasks/users-tasks.component.ts
+++ src/app/component/users-tasks/users-tasks.component.ts
@@ -15,39 +15,13 @@ export class UsersTasksComponent {
   @Input({ required: true }) name!: string;
   addTask = false;
 
-  dummyTasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
-
   get selectedUserTasks() {
     return this.dummyTasks.filter((task) => task.userId === this.id);
   }
 
   onCompleteTask(id: string) {
     console.log('cheeeee', id);
-    this.dummyTasks = this.dummyTasks.filter((task) => {
+    dummyTasks = this.dummyTasks.filter((task) => {
       console.log("task id -> ", task.id)
       console.log("user id -> ", id)
       console.log("condition running : ",task.id !== id);
